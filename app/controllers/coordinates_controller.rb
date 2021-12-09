bacab8dd48678a308b01a0e3fb6e872588e5c882
Diana Baiadori <diana.baiadori@gmail.com>
2021-12-09 16:41:43 +0100

    started the project and added a form

---
+++ app/controllers/coordinates_controller.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+require 'json'
+
+class CoordinatesController < ApplicationController
+  def new
+    @coordinate = Coordinate.new
+  end
+
+  def create
+    latitude = params['coordinate']['latitude']
+    longitude = params['coordinate']['longitude']
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=#{latitude},#{longitude}&access_token=pk.eyJ1IjoiZHBiYWlhIiwiYSI6ImNrdmt1eDU3cTBmZWYyeG81ZDVjODVjNTMifQ.voVa-yQjGv5WxY4cXY1_ZA"
+    museums_serialised = URI.open(url).read
+    museums = JSON.parse(museums_serialised)
+
+    raise
+
+  end
+
+  # private
+  #
+  # def coordinate_params
+  #   params.require(:coordinate).permit(:latitude, :longitude)
+  # end
+end
