396fec88a724ca2797f5b2fd5b637d1ebf0458e9
Diana Baiadori <diana.baiadori@gmail.com>
2021-12-09 19:01:15 +0100

    feature: general functionality is working, needs to fix so it finds the postcode no matter where it is

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -2,25 +2,36 @@ require 'open-uri'
 require 'json'
 
 class CoordinatesController < ApplicationController
-  def new
-    @coordinate = Coordinate.new
-  end
-
-  def create
-    latitude = params['coordinate']['latitude']
-    longitude = params['coordinate']['longitude']
+  def index
+    latitude = params['lat']
+    longitude = params['lng']
     url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=#{latitude},#{longitude}&access_token=pk.eyJ1IjoiZHBiYWlhIiwiYSI6ImNrdmt1eDU3cTBmZWYyeG81ZDVjODVjNTMifQ.voVa-yQjGv5WxY4cXY1_ZA"
     museums_serialised = URI.open(url).read
     museums = JSON.parse(museums_serialised)
+    @hash = {}
 
-    museums["features"]
-    raise
+    museums["features"].each do |museum|
+      name = museum["text"]
 
+      if museum["context"][0]["id"].start_with?("postcode")
+        # Need to adapt above so that it looks for all the context and sees if any of them has a postcode
+        # (it's not always the first one)
+
+        postcode = museum["context"][0]["text"]
+        # pattern = /(\D*),.*\D,\D*(\d*)/
+        # whole_info = museum["place_name"]
+        # match_data = whole_info.match(pattern)
+        # name = match_data[1]
+        # postcode = match_data[2]
+        if @hash.key?(postcode)
+          @hash[postcode].append(name)
+          raise
+        else
+          @hash[postcode] = [name]
+        end
+
+      end
+
+    end
   end
-
-  # private
-  #
-  # def coordinate_params
-  #   params.require(:coordinate).permit(:latitude, :longitude)
-  # end
 end
