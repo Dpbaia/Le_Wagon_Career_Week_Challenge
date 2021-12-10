ba8bfb88f837771cd9276e7d7943ce0c7cd353b7
Diana Baiadori <diana.baiadori@gmail.com>
2021-12-10 10:16:10 +0100

    fix: now will find postcode no matter where it is

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -12,25 +12,26 @@ class CoordinatesController < ApplicationController
 
     museums["features"].each do |museum|
       name = museum["text"]
+      museum["context"].each do |info|
+        if info["id"].start_with?("postcode")
+          # Need to adapt above so that it looks for all the context and sees if any of them has a postcode
+          # (it's not always the first one)
 
-      if museum["context"][0]["id"].start_with?("postcode")
-        # Need to adapt above so that it looks for all the context and sees if any of them has a postcode
-        # (it's not always the first one)
+          postcode = info["text"]
+          # pattern = /(\D*),.*\D,\D*(\d*)/
+          # whole_info = museum["place_name"]
+          # match_data = whole_info.match(pattern)
+          # name = match_data[1]
+          # postcode = match_data[2]
+          if @hash.key?(postcode)
+            @hash[postcode].append(name)
+          else
+            @hash[postcode] = [name]
+          end
 
-        postcode = museum["context"][0]["text"]
-        # pattern = /(\D*),.*\D,\D*(\d*)/
-        # whole_info = museum["place_name"]
-        # match_data = whole_info.match(pattern)
-        # name = match_data[1]
-        # postcode = match_data[2]
-        if @hash.key?(postcode)
-          @hash[postcode].append(name)
-          raise
-        else
-          @hash[postcode] = [name]
         end
-
       end
+
 
     end
   end
